734875eb6f41dcf7841c67dc1e3a7244823d1ddb
Ângelo Francisco <mano.cross1932@gmail.com>
2019-11-05 09:53:33 -0300

    Basics examples

---
+++ biogenetica.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+entrada = open("16s_bacteria.fasta").read()
+saida = open("bacteria.html","w")
+
+cont = {}
+
+for i in ['A','T', 'C', 'G']:
+    for j in ['A','T', 'C', 'G']:
+        cont[i+j] = 0
+
+entrada = entrada.replace("\n","")
+
+for k in range(len(entrada)-1):
+    cont[entrada[k]+entrada[k+1]] += 1
+
+print(cont)
